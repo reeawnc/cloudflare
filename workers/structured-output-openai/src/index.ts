ba4fb112c7a3a76609da836ce3769c997fd02499
Andy Jessop <ajessop@cloudflare.com>
2025-02-17 13:07:02 +0100

    Add various workers and templates

---
+++ workers/structured-output-openai/src/index.ts
@@ -0,0 +1,37 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { generateObject } from "ai";
+import z from "zod";
+import type { Env } from "./types/env.ts";
+import type { Variables } from "./types/hono.ts";
+import { createOpenAI } from "@ai-sdk/openai";
+
+const app = new Hono<{ Bindings: Env; Variables: Variables }>();
+app.use(cors());
+
+app.post("/", async (c) => {
+	const { prompt } = (await c.req.json()) as { prompt: string };
+	const openai = createOpenAI({
+		apiKey: c.env.OPENAI_API_KEY,
+	});
+
+	const { object } = await generateObject({
+		model: openai("gpt-4o-mini"),
+		schema: z.object({
+			recipe: z.object({
+				name: z.string(),
+				ingredients: z.array(
+					z.object({ name: z.string(), amount: z.string() }),
+				),
+				steps: z.array(z.string()),
+			}),
+		}),
+		prompt,
+	});
+
+	return c.json(object);
+});
+
+export default {
+	fetch: app.fetch,
+} satisfies ExportedHandler<Env>;
