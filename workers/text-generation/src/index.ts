9e33d729176f5ea16272532f82ee4bd3336f981c
Andy Jessop <ajessop@cloudflare.com>
2025-02-18 09:51:44 +0100

    renaming some workers

--- workers/text-generation/src/index.ts
+++ workers/text-generation/src/index.ts
@@ -1,10 +1,9 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { generateObject } from "ai";
-import z from "zod";
+import { generateText } from "ai";
 import type { Env } from "./types/env.ts";
 import type { Variables } from "./types/hono.ts";
-import { createWorkersAI } from "workers-ai-provider";
+import { createOpenAI } from "@ai-sdk/openai";
 import { authApiKey } from "../../../libs/middleware/src/auth-api-key";
 
 const app = new Hono<{ Bindings: Env; Variables: Variables }>();
@@ -13,23 +12,16 @@ app.use("*", authApiKey);
 
 app.post("/", async (c) => {
 	const { prompt } = (await c.req.json()) as { prompt: string };
-	const workersai = createWorkersAI({ binding: c.env.AI });
+	const openai = createOpenAI({
+		apiKey: c.env.OPENAI_API_KEY,
+	});
 
-	const { object } = await generateObject({
-		model: workersai("@cf/meta/llama-3.3-70b-instruct-fp8-fast"),
-		schema: z.object({
-			recipe: z.object({
-				name: z.string(),
-				ingredients: z.array(
-					z.object({ name: z.string(), amount: z.string() }),
-				),
-				steps: z.array(z.string()),
-			}),
-		}),
+	const result = await generateText({
+		model: openai("gpt-4o-mini"),
 		prompt,
 	});
 
-	return c.json(object);
+	return c.json(result);
 });
 
 export default {
