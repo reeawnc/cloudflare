5f279158dc0e3e56339e4215b767f643a41ef596
Andy Jessop <ajessop@cloudflare.com>
2025-03-17 13:21:24 +0100

    Update text-generation-stream model

---
+++ demos/text-generation-stream/src/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { streamText } from "ai";
+import { createWorkersAI } from "workers-ai-provider";
+import type { Env } from "./types/env.ts";
+import type { Variables } from "./types/hono.ts";
+
+const app = new Hono<{ Bindings: Env; Variables: Variables }>();
+app.use(cors());
+
+app.post("/", async (c) => {
+	const { prompt } = (await c.req.json()) as { prompt: string };
+	const workersai = createWorkersAI({ binding: c.env.AI });
+	const model = workersai("@cf/meta/llama-3.3-70b-instruct-fp8-fast");
+
+	const result = streamText({
+		model,
+		prompt,
+	});
+
+	return result.toDataStreamResponse({
+		headers: {
+			"Content-Type": "text/x-unknown",
+			"content-encoding": "identity",
+			"transfer-encoding": "chunked",
+		},
+	});
+});
+
+export default {
+	fetch: app.fetch,
+} satisfies ExportedHandler<Env>;
