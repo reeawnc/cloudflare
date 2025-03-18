2629723a0d6d4fe3ee43e69c33129688046e048c
Andy Jessop <ajessop@cloudflare.com>
2025-03-18 12:13:59 +0100

    chore: remove unnecessary files

--- demos/structured-output-node/src/types/env.ts
+++
@@ -1,7 +0,0 @@
-export interface Env {
-	ENVIRONMENT: "production" | "development" | "staging";
-	OPENAI_API_KEY: string;
-	AI: Ai;
-	CLOUDFLARE_API_TOKEN: string;
-	CLOUDFLARE_ACCOUNT_ID: string;
-}
