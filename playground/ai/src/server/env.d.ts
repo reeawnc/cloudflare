f077173b7ac9a046cace948ce27646b140a140e5
Sunil Pai <spai@cloudflare.com>
2025-06-03 17:37:36 +0530

    Revert "get a bunch of stuff working for ai playground"
    This reverts commit 308d34babebc171036b8e3a0bb7c2c03dee619c2.

--- playground/ai/src/server/env.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Cloudflare {
-	export interface Env {
-		AI: Ai;
-		CLOUDFLARE_API_TOKEN: string;
-		CLOUDFLARE_ACCOUNT_ID: string;
-	}
-}
-
-interface Env extends Cloudflare.Env {}
